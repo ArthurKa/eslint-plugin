eba0150137be82dfdda16ef326912a3c199e318f
ArthurKa <arthur.katruk@gmail.com>
2023-01-28 16:34:21 +0200

    feat: add recommended config

---
+++ configs/recommended.js
@@ -0,0 +1,8 @@
+// @ts-check
+'use strict';
+
+module.exports = ((/** @type {import('eslint').Linter.Config} */ e) => e)({
+  rules: {
+    'arthurka/no-space-between-empty-curlies': 'warn',
+  },
+});
