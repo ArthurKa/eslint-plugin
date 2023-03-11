96a8f3c2ab89e07015d207e0cca78d9889f4e1c7
ArthurKa <arthur.katruk@gmail.com>
2023-03-11 23:13:52 +0200

    feat: disable `no-unused-imports` for `*.jsx` and `.tsx` files

--- src/configs.ts
+++ src/configs.ts
@@ -5,4 +5,12 @@ export const recommended: Linter.Config = {
   rules: Object.fromEntries(
     Object.values(rules).map(({ ruleName, ruleEntry }) => [`arthurka/${ruleName}`, ruleEntry]),
   ),
+  overrides: [
+    {
+      files: ['*.jsx', '*.tsx'],
+      rules: {
+        'arthurka/no-unused-imports': 'off',
+      },
+    },
+  ],
 };
