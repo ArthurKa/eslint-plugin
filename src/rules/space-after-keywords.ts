d7a562f9e34bbdf6ceae5d9d78b8fd7ff5549fbd
ArthurKa <arthur.katruk@gmail.com>
2023-03-11 22:33:11 +0200

    feat: remove `type` from `space-after-keywords`

--- src/rules/space-after-keywords.ts
+++ src/rules/space-after-keywords.ts
@@ -3,5 +3,5 @@ import { addSpaceForKeyword, createPluginRule } from '../utils';
 export default createPluginRule({
   ruleName: 'space-after-keywords',
   type: 'layout',
-  create: addSpaceForKeyword('after', ['satisfies', 'as', 'type']),
+  create: addSpaceForKeyword('after', ['satisfies', 'as']),
 });
